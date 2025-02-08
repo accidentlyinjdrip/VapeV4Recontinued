893bff87d8de3417fc5e56a340bfe4bbdd6aa5e9
accidentlyinjdrip <dtconinject@outlook.com>
2025-02-08 13:08:42 -0500

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -719,7 +719,6 @@ run(function()
 		AttackEntity = Knit.Controllers.SwordController.sendServerRequest,
 		BeePickup = Knit.Controllers.BeeNetController.trigger,
 		ConsumeBattery = debug.getproto(debug.getproto(Knit.Controllers.BatteryController.KnitStart, 1), 1),
-		CannonAim = debug.getproto(Knit.Controllers.CannonController.startAiming, 5),
 		CannonLaunch = Knit.Controllers.CannonHandController.launchSelf,
 		ConsumeItem = debug.getproto(Knit.Controllers.ConsumeController.onEnable, 1),
 		ConsumeSoul = Knit.Controllers.GrimReaperController.consumeSoul,
